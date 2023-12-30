86b513ee547b770f1a7e5482757e00ac6cef5270
Hennadii Borysevych <genuch3@gmail.com>
2023-12-30 23:27:45 +0100

    feat:forms styles and html

--- forms/review/views.py
+++ forms/review/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
+from .forms import ReviewForm
 
 
-def review():
-    pass
+def review(req):
+    if req.method == 'POST':
+        form = ReviewForm(req.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+
+            return HttpResponseRedirect('/thank-you')
+    else:
+        form = ReviewForm()
+
+    return render(req, 'review/review.html', {"form": form})
+
+
+def thank_you(req):
+    return render(req, 'review/thank_you.html')
