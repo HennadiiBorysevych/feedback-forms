dfb5294cf8cf22617dd65ad5f07679173bc3c7d2
Hennadii Borysevych <genuch3@gmail.com>
2023-12-31 13:00:50 +0100

    feat:form moels

--- forms/review/views.py
+++ forms/review/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 # Create your views here.
 
 from .forms import ReviewForm
+from .models import Review
+
+
+class ReviewView(View):
+    def get(self, req):
+        form = ReviewForm()
+        return render(req, 'review/review.html', {"form": form})
+
+    def post(self, req):
+        form = ReviewForm(req.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thank-you')
 
 
 def review(req):
     if req.method == 'POST':
-        form = ReviewForm(req.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
+        existing_model = Review.objects.get(pk=1)
+        form = ReviewForm(req.POST, instance=existing_model)
 
+        if form.is_valid():
+            form.save()
             return HttpResponseRedirect('/thank-you')
     else:
         form = ReviewForm()
